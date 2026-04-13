2a764ed79691214f06831f3b29c7114298cc29f0
swamisamarth526-design <swamisamarth526@gmail.com>
2026-04-13 16:31:28 +0530

    Add Data

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: process.env.NODE_ENV === "production" ? true : "http://localhost:5173",
+    credentials: true,
+};
 
 app.use(cors(corsOptions));
 
